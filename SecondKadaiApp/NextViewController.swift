ef4d10e79d9900213bcad18bdaf11e9652ba2929
peiwen.ryuu <liu@i-enter.co.jp>
2021-04-13 11:44:10 +0900

    NextViewControllerを追加しました。

---
+++ SecondKadaiApp/NextViewController.swift
@@ -0,0 +1,20 @@
+//
+//  NextViewController.swift
+//  SecondKadaiApp
+//
+//  Created by Liu Peiwen on 2021/04/13.
+//
+
+import UIKit
+
+class NextViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
