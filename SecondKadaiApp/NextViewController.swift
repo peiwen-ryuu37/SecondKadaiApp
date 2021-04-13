ab7673dee5f3525c7e144bf7dd43d2fdc2951e70
peiwen.ryuu <liu@i-enter.co.jp>
2021-04-13 12:38:41 +0900

    NextViewControllerでの値渡し、画面遷移メソッド追加しました。

--- SecondKadaiApp/NextViewController.swift
+++ SecondKadaiApp/NextViewController.swift
@@ -8,11 +8,17 @@
 import UIKit
 
 class NextViewController: UIViewController {
+    @IBOutlet weak var userNameLabel: UILabel!
+    
+    var userName: String = "none"
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        self.userNameLabel.text = "こんにちは、\(self.userName)さん"
+        
     }
     
 
--- SecondKadaiApp/ViewController.swift
+++ SecondKadaiApp/ViewController.swift
@@ -8,10 +8,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var nameTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let nextViewController: NextViewController = segue.destination as! NextViewController
+        nextViewController.userName = nameTextField.text ?? "none"
+    }
+    
+    @IBAction func unwind(_ segue: UIStoryboardSegue) {
+        
     }
 
 
